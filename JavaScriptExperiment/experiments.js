b9dfea42b0eb07aaf437c796cfc49a9b5c1fbe37
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-06-25 11:35:02 +0700

    Hello my name

--- JavaScriptExperiment/experiments.js
+++ JavaScriptExperiment/experiments.js
@@ -1,1 +1,22 @@
 console.log("HELLO WORLD");
+
+
+// const timesToRepeat = 5;
+// const dog = "🐕"
+
+// let word = "";
+// for (let i = 0; i < timesToRepeat; i++) {
+//   word += dog;
+
+// }
+
+// console.log("".padStart(10, "🐕" ));
+
+
+// string format
+
+let name = "Yuujin"
+
+// console.log("Hello " + name + "!");
+
+console.log(`Hello ${name}!`); // in order to use format string, use backticks and ${}
