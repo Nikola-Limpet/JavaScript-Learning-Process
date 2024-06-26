06dab825aaa0a69269e459697e2d20aaacd0b05a
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-06-26 12:51:10 +0700

    new on function and scope

--- JavaScriptExperiment/experiments.js
+++ JavaScriptExperiment/experiments.js
@@ -20,3 +20,14 @@ let name = "Yuujin"
 // console.log("Hello " + name + "!");
 
 console.log(`Hello ${name}!`); // in order to use format string, use backticks and ${}
+
+
+// function 
+function addfive(number) {
+  let someVar = "You can't see me outside this function";
+  console.log(someVar);
+  return number + 5
+}
+
+console.log(addfive(5));
+console.log(addfive(someVar));  // someVar is out of scope it no long use here outside the function addfive
