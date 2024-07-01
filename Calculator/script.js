4cba53ecde7fc89daa24403062cc5f0274aa40d8
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-01 14:22:08 +0700

    finnish html and css

---
+++ Calculator/script.js
@@ -0,0 +1,13 @@
+// TODO: implement a calculator
+// 1. The calculator should be able to perform addition, subtraction, multiplication, and division
+// 2. The calculator should be able to display the result
+// 3. The calculator should be able to clear the display
+// 4. The calculator should be able to handle decimal numbers
+// 5. The calculator should be able to handle negative numbers
+// 6. The calculator should be able to handle keyboard input
+
+let buffer = '0';
+
+// function init() {
+
+// }
