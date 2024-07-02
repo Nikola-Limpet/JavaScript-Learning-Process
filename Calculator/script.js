53036c3f496248df120cc3152715dac8ba6db0c5
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-02 16:37:54 +0700

    finnish basic cal

--- Calculator/script.js
+++ Calculator/script.js
@@ -7,7 +7,73 @@
 // 6. The calculator should be able to handle keyboard input
 
 let buffer = '0';
+const screen = document.querySelector('.screen-display');
 
+function buttonClick(value) {
+  // handle number and symbol
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  rerender();
+}
+
+function handleNumber(number) {
+  if (buffer === '0') {
+    buffer = number
+  } else {
+    buffer += number
+  }
+}
+
+function handleSymbol(number) {
+  switch(number) {
+    case 'C':
+      buffer = '0';
+      break;
+    case '=':
+      if (buffer === '0') {
+        break;
+      } 
+      else if (buffer.includes('x')) {
+        buffer = eval(buffer.replace('x', '*'));
+        break;
+      }
+      else {
+        buffer = eval(buffer);
+      }
+      break;
+    case '🔙': 
+      if (buffer.length === 1) {
+        buffer = '0';
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    default:
+      buffer += number;
+  } 
+}
+
+function init() {
+    document.querySelectorAll('.button').forEach(button => {
+    button.addEventListener('click', function(event) {
+      buttonClick(event.target.innerText);
+    });
+    });
+}
 // function init() {
-
+//   let buttons = documentory.querySelectorAll('.button');
+//   for (let i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener('click', function(event) {
+//     buttonClick(event.target.innerText);
+//     });
+//   }
 // }
+  
+function rerender() {
+  screen.innerText = buffer;
+}
+
+init();
