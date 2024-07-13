155e5e0310d7a4b8e572596bfbd04d457c068432
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-13 14:01:01 +0700

    prototype and this

--- JavaScriptExperiment/function.js
+++ JavaScriptExperiment/function.js
@@ -36,3 +36,63 @@ let teacher = "Kyle";
 }) ();  // in the last: ()    to invoked the function
 
 console.log(teacher);  // Kyle
+
+function formatStr (str) {
+  {
+    let prefix, rest;
+    prefix = str.slice(0, 3);
+    rest = str.slice(3);
+    str = prefix.toUpperCase() + rest;
+  }
+  if (/^FOO:/.test(str)) { // test() is a method of regExp for testing whether input str is in
+    // the pattern or not
+
+    return str;   
+  }
+  return str.slice( 4 );
+}
+
+  // Closer is when a function remember the vari outside of it. even if u dont pass that elsewhere
+function ask(question) {
+  setTimeout(function waitASec(){
+    console(question);
+  },100);
+}
+
+ask('what is closer?');
+
+// what is closer?
+
+
+
+// Prototypes 
+
+/* a this-aware function can thus have a different context each time it's call
+, which makes it more flexible & reusable */
+
+// workShop is an object type so we can call element inside is by usig this similar to workshop.this
+var workShop = {
+  teacher: "Kyle", 
+  ask(question) {
+    console.log(this.teacher, question);
+   // remember that we can use this.ask or this.teacher
+  },
+}
+
+workShop.ask("what is implicit binding?");
+
+// Kyle, what is implicit binding?
+
+function Workshop(teacher) {
+  this.teacher = teacher;
+}
+Workshop.prototype.ask = function(question) {   // like we have object workshop and inside that
+  // object we create function ask but instead we use prototype 
+  console.log(this.teacher, question);
+};
+
+var deepJS = new Workshop("Kyle");
+var reactJS = new Workshop("Suzy");
+
+deepJS.ask("Isn't prototype is a class?");
+reactJS.ask("Is prototype is Ugly");
