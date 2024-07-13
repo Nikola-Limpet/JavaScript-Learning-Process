93eaea590bd0725cd72ab5c129b1f5ab248aac04
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-13 15:09:06 +0700

    add book ex using class

--- JavaScriptExperiment/exercises/three-pillars/ex.js
+++ JavaScriptExperiment/exercises/three-pillars/ex.js
@@ -2,29 +2,33 @@ class Bookshelf {
 	constructor() {
 		this.favoriteBooks = [];
 	}
-
+	addFavoriteBook(bookName) {
+		if (!bookName.includes("Great")) {
+			this.favoriteBooks.push(bookName);
+		}
+	}
+	printFavoriteBooks() {
+		console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);	// this.favoriteBooks.length is number is not iteratable
+		for (let bookName of this.favoriteBooks) {	
+			console.log(bookName);
+		}
+	}
+}	
 	// TODO: define methods `addFavoriteBook(..)`
 	// and `printFavoriteBooks()`
-}
-
-function addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		favoriteBooks.push(bookName);
-	}
-}
-
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
-
-function loadBooks( /* .. */ ) {
+function loadBooks(myBooks) {
 	// TODO: call fakeAjax( .. );
+	fakeAjax(BOOK_API, (bookNames) => { // use arrow function instead of function(bookNames)
+		for(let bookName of bookNames) {
+			myBooks.addFavoriteBook(bookName);
+		}
+		myBooks.printFavoriteBooks();
+	});
 }
 
 var BOOK_API = "https://some.url/api";
+var myBooks = new Bookshelf();
+loadBooks(myBooks);
 
 
 // ***********************
