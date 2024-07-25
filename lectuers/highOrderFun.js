261ebc5b9e6c2516ec7a52aa49bc5f2d07a56abf
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-25 06:38:44 +0000

    Jul 25, 2024, 1:38 PM

--- lectuers/highOrderFun.js
+++ lectuers/highOrderFun.js
@@ -2,10 +2,10 @@
 function copyArrManipulate(arr, instruction) {
   const output = []
   for (let i = 0; i < arr.length; i++) {
-  
+    output.push(instruction(arr[i]))
   }
-}
+};
 
-function multiBy2(input) { return input *  2}
+function multiBy2(input) { return input *  2};
 
-const result = copyArrManipulate()
+const result = copyArrManipulate([1,2,3], multiBy2);
