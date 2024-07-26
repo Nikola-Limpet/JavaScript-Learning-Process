bd7c5f3be9d42368559ac710f6fa56b5215a2b51
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-26 05:13:04 +0000

    Jul 26, 2024, 12:13 PM

--- lectures/arrowFun.js
+++ lectures/arrowFun.js
@@ -7,3 +7,57 @@
 // const output = mul2(3); // 6
 
 
+// Type JavaScript here and click "Run Code" or press Ctrl + s
+console.log('Hello, world!');
+
+
+// Challenge 1
+function addTwo(num) {
+	return num  + 2;
+} 
+
+// To check if you've completed it, uncomment these console.logs!
+// console.log(addTwo(3));
+// console.log(addTwo(10));
+
+
+// Challenge 2
+function addS(word) {
+	return word + "s";
+}
+
+// uncomment these to check your work
+// console.log(addS('pizza'));
+// console.log(addS('bagel'));
+
+
+// Challenge 3
+function map(array, callback) {
+  const output = [];
+	for (let i = 0; i < array.length; i++) {
+    output.push(callback(array[i]));
+  };
+  return output;
+}
+
+//console.log(map([1, 2, 3], addTwo));
+
+
+// Challenge 4
+function forEach(array, callback) {
+  const output = [];
+	for(let i = 0; i < array.length; i++) {
+    output.push(callback(array[i]));
+  };
+  
+}
+
+// see for yourself if your forEach works!
+
+
+// Challenge 5
+function mapWith(array, callback) {
+  const output = [];
+  forEach(array, callback);
+  return output;
+}
