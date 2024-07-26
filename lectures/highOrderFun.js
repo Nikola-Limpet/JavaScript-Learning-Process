086042d1cffaf286d838116f74e1dd5ecb7cbb0b
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-26 05:07:56 +0000

    Jul 26, 2024, 12:07 PM

--- lectures/highOrderFun.js
+++ lectures/highOrderFun.js
@@ -22,6 +22,7 @@
 // const result = copyArrManipulate([1,2,3], input => input * 2);
 // console.log(result)
 
+// challenge 3
 const addTwo = input => input +2;
 
 function map(arr, callBack) {
@@ -33,6 +34,7 @@ function map(arr, callBack) {
 }
 
 console.log(map([1,2,3], addTwo)) // [3,4,5]
+
 
 
 // Challenge 4
@@ -54,3 +56,6 @@ forEach(letter, function(char) {
 console.log(alphabet); // abcdefghij
 
 // Challenge 5
+// In challenge 3, you've created a function called map. 
+// In this challenge, you're going to rebuild the map function by creating a function called mapWith.
+// This time you're going to use forEach inside of mapWith instead of using a for loop.
