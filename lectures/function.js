09097e3d9f99d3068bbd1c2e101951c9a2f73733
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-28 06:42:34 +0000

    Jul 28, 2024, 1:42 PM

--- lectures/function.js
+++ lectures/function.js
@@ -9,10 +9,16 @@ function outer() {
 const myNewFunction = outer();
 myNewFunction();
 myNewFunction();
+// Output:1,2 
 
 const anotherNewFunction = outer();
 anotherNewFunction();
 anotherNewFunction();
+// Output: 1,2
+
+//Since it is an individual backpack the counter variable use only inside the scope of the function outer()
+// Unless the counter decleared in the global execution context then the output must be 1,2,3,4
+
 
 /*  What can we call it backpack?
 
