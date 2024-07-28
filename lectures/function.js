e4bd5b18d3d7bffd81441d50b90d888e0d07404b
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-28 05:39:29 +0000

    Jul 28, 2024, 12:39 PM

---
+++ lectures/function.js
@@ -0,0 +1,12 @@
+// Calling a function outside of the function call in which it was defined 
+
+function outer() {
+  let counter = 0;
+  function incrementCounter() { counter ++;}
+  return incrementCounter;
+}
+
+const myNewFunction = outer();
+myNewFunction();
+myNewFunction();
+
