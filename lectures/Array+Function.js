0eccb2c8f3dd838d47f4bfd9b61d4b51ef8336e4
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-31 02:01:34 +0000

    Jul 31, 2024, 9:01 AM

--- lectures/Array+Function.js
+++ lectures/Array+Function.js
@@ -11,3 +11,11 @@
 // index (Optional): The index of the current element being processed in the array.
 // array (Optional): The array map was called upon.
 // thisArg (Optional): Value to use as this when executing callback.
+
+// Return value
+
+const a = [1, 2, 3, 4, 5];
+const b = a.map((x) => x * 2);
+console.log(b); // [2, 4, 6, 8, 10]
+
+// const b = a.map(item, index, array) => { console.log(item, index, array) }
