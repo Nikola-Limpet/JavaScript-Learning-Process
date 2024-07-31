ad89da8c6b166d32539f2049a00605afa017b870
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-31 01:57:15 +0000

    Jul 31, 2024, 8:57 AM

---
+++ lectures/Array+Function.js
@@ -0,0 +1,13 @@
+
+
+// map() function
+// The map() function creates a new array with the results of calling a provided function on every element in the calling array.
+
+// Syntax
+// map(callback(element, index, array), thisArg)  
+// Parameters
+// callback: Function that produces an element of the new Array, taking three arguments:
+// element: The current element being processed in the array.
+// index (Optional): The index of the current element being processed in the array.
+// array (Optional): The array map was called upon.
+// thisArg (Optional): Value to use as this when executing callback.
