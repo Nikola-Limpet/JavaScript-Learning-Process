27d62426a9a34a0058d279480167f011594e878b
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-08-01 04:00:36 +0000

    Aug 1, 2024, 11:00 AM

---
+++ lectures/oop.js
@@ -0,0 +1,12 @@
+// Objects - stores functions with their associated data
+
+// this is the principle of encapsulation- and it's going to tranform the way 
+//you write code
+
+const user1 = {
+  name: 'will', 
+  score : 3,
+  increment: () => user1.score++
+};
+
+user1.increment(); // user1.score = 4
