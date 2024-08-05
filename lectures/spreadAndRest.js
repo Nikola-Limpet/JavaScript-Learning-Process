490ab7e3ce50337c83c08d6d253a70356e67a1d3
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-08-05 04:55:12 +0000

    Aug 5, 2024, 11:55 AM

--- lectures/spreadAndRest.js
+++ lectures/spreadAndRest.js
@@ -8,3 +8,16 @@ const car  = { wheels : 4 };
 const flyingCar = { ...flying, ...car }; // { wings: 2, wheels: 4 }
 
 console.log(flyingCar);
+
+
+// another example of spread operator
+let myCar = [ "BMW", "Audi", "Toyota" ];
+myCar = [...myCar, "Mercedes", "Ford"];
+console.log(myCar); // ['BMW', 'Audi', 'Toyota', 'Mercedes', 'Ford']
+
+
+// another example of spread operator
+const greeting = 'Hello';
+const arraysOfChars = [...greeting]; // ['H', 'e', 'l', 'l', 'o']
+console.log(arraysOfChars);
+
