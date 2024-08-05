62d148096a3def49b6f3a57be408739ce442d1e2
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-08-05 03:43:36 +0000

    Aug 5, 2024, 10:43 AM

--- lectures/ForOfLoopsAndObjects.js
+++ lectures/ForOfLoopsAndObjects.js
@@ -8,3 +8,13 @@ sportCar.speed = "fast";
 console.log("The sportCar object: " , sportCar);
 
 console.log('------- for-in is unreliable ----------');
+for (prop in sportCar) {
+  console.log(prop);
+}
+console.log("Iteraing over the object and prototype properties");
+
+console.log('------- for-of is reliable ----------');
+for (prop of Object.keys(sportCar)) {
+  console.log(prop);
+}
+console.log("Iteraing over the object properties only");
