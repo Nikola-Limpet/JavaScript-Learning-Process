faac4647623dc272838896b429abebdbb8ceda76
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-08-05 03:42:06 +0000

    Aug 5, 2024, 10:42 AM

--- lectures/ForOfLoopsAndObjects.js
+++ lectures/ForOfLoopsAndObjects.js
@@ -3,3 +3,8 @@ const car = {
   steering : true,
   speed : "slow"
 }
+const sportCar = Object.create(car);
+sportCar.speed = "fast";
+console.log("The sportCar object: " , sportCar);
+
+console.log('------- for-in is unreliable ----------');
