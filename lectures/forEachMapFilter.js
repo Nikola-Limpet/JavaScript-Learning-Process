6e93d6d5947ed17bc6e4453a3ffb44ad969aeb67
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-08-05 04:27:45 +0000

    Aug 5, 2024, 11:27 AM

---
+++ lectures/forEachMapFilter.js
@@ -0,0 +1,18 @@
+
+// ForEach built-in function
+
+const fruit = ['apple', 'banana', 'grapes', 'mango', 'orange'];
+
+fruit.forEach((item, index) => {
+  console.log(index, item);
+  }
+);
+
+let nums = [0 , 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+nums.filter((nums) => {
+  return nums >= 2;
+});
+
+console.log(nums);
+
